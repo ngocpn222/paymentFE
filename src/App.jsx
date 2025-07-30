70bba4542d892e5622c91afa25cd9f8b186c3b77
NguyenTu11 <tunguyenvan1110@gmail.com>
2025-07-30 19:37:56 +0700

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import './App.css';
 import Login from "./components/Auth/Login";
 import Register from "./components/Auth/Register";
 import StudentList from "./components/Students/StudentList";
